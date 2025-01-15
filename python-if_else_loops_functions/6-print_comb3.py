132da82172f1b340f19fdf338cce24c8f206981c
adoniskoco <10028@holbertonstudents.com>
2025-01-15 13:10:43 +0000

    Added files

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
-for i in range(0, 9):
-    for j in range (i + 1, 10):
-        number = str(i) + str(j)
-        if number == "89":
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=", ")
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            if i != 8 and i != 9:
+                print("{}{}, ".format(i, j), end="")
+            else:
+                print("{}{}".format(i, j), end="" "\n")
